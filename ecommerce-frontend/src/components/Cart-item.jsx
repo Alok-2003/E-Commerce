b9c06b390721dc5b6e26c30afdab5c9e93f4e9c3
Alok kumar yadav <alok953280@gmail.com>
2024-03-13 22:59:27 +0530

    done

--- ecommerce-frontend/src/components/Cart-item.jsx
+++ ecommerce-frontend/src/components/Cart-item.jsx
@@ -1,15 +1,19 @@
-type CartItemProps = {
-    cartItem: CartItem;
-    incrementHandler: (cartItem: CartItem) => void;
-    decrementHandler: (cartItem: CartItem) => void;
-    removeHandler: (id: string) => void;
-};
+// type CartItemProps = {
+//     cartItem: CartItem;
+//     incrementHandler: (cartItem: CartItem) => void;
+//     decrementHandler: (cartItem: CartItem) => void;
+//     removeHandler: (id: string) => void;
+// };
 
-const CartItem = ({
-    cartItem,
-    incrementHandler,
-    decrementHandler,
-    removeHandler,
-}: CartItemProps) => {
+// const CartItem = ({
+//     cartItem,
+//     incrementHandler,
+//     decrementHandler,
+//     removeHandler,
+// }: CartItemProps) => {
+//     return <div>
+//         Cart Items
+//     </div>
+// };
 
-export default CartItem;
+// export default CartItem;
--- ecommerce-frontend/src/components/Header.tsx
+++ ecommerce-frontend/src/components/Header.tsx
@@ -1,63 +1,36 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
-import {FaSearch,FaShoppingBag,FaSignInAlt,FaUser,FaSignOutAlt} from "react-icons/fa";
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaUser, FaSignOutAlt, FaSign } from "react-icons/fa";
 import { Link } from "react-router-dom"
+
+const user = { _id: "" }
 
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const logoutHandler = async () => {
-        try {
-          await signOut();
-          toast.success("Sign Out Succesfully");
-          setIsOpen(false);
-        } catch (error) {
-          toast.error("Sign Out Fail");
-        }
-      };
+  return (
+    <nav className="header">
+      <Link to={"/"}>
+        HOME
+      </Link>
+      <Link to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
 
-    return (
-        <nav className="header"> 
-            <Link onClick={() => setIsOpen(false)} to={"/"}>
-                HOME
-            </Link>
-            <Link onClick={() => setIsOpen(false)} to={"/search"}>
-                <FaSearch />
-            </Link>
-            <Link onClick={() => setIsOpen(false)} to={"/cart"}>
-                <FaShoppingBag />
-            </Link>
+      {
+        user?._id ? (
+          <>
+          </>
+        ) : <Link to={"/login"}>
+        <FaSign />
+      </Link>
+      }
 
-            {user?._id ? (
-        <>
-          <button onClick={() => setIsOpen((prev) => !prev)}>
-            <FaUser />
-          </button>
-          <dialog open={isOpen}>
-            <div>
-              {user.role === "admin" && (
-                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
-                  Admin
-                </Link>
-              )}
-
-              <Link onClick={() => setIsOpen(false)} to="/orders">
-                Orders
-              </Link>
-              <button onClick={logoutHandler}>
-                <FaSignOutAlt />
-              </button>
-            </div>
-          </dialog>
-        </>
-      ) : (
-        <Link to={"/login"}>
-          <FaSignInAlt />
-        </Link>
-      )}
-        </nav>
-    )
+    </nav>
+  )
 }
 
 export default Header
