8de02d955ba09e0e629ab82a844c00eb49429121
Alok kumar yadav <alok953280@gmail.com>
2024-03-25 22:08:56 +0530

    done

--- ecommerce-frontend/src/pages/Shipping.tsx
+++ ecommerce-frontend/src/pages/Shipping.tsx
@@ -59,3 +59,66 @@ const Shipping = () => {
   useEffect(() => {
     if (cartItems.length <= 0) return navigate("/cart");
   }, [cartItems]);
+  
+  return (
+    <div className="shipping">
+      <button className="back-btn" onClick={() => navigate("/cart")}>
+        <BiArrowBack />
+      </button>
+
+      <form onSubmit={submitHandler}>
+        <h1>Shipping Address</h1>
+
+        <input
+          required
+          type="text"
+          placeholder="Address"
+          name="address"
+          value={shippingInfo.address}
+          onChange={changeHandler}
+        />
+
+        <input
+          required
+          type="text"
+          placeholder="City"
+          name="city"
+          value={shippingInfo.city}
+          onChange={changeHandler}
+        />
+
+        <input
+          required
+          type="text"
+          placeholder="State"
+          name="state"
+          value={shippingInfo.state}
+          onChange={changeHandler}
+        />
+
+        <select
+          name="country"
+          required
+          value={shippingInfo.country}
+          onChange={changeHandler}
+        >
+          <option value="">Choose Country</option>
+          <option value="india">India</option>
+        </select>
+
+        <input
+          required
+          type="number"
+          placeholder="Pin Code"
+          name="pinCode"
+          value={shippingInfo.pinCode}
+          onChange={changeHandler}
+        />
+
+        <button type="submit">Pay Now</button>
+      </form>
+    </div>
+  );
+};
+
+export default Shipping;
