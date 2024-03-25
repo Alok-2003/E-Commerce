ce0e28dc778f006ff2b86161cbe75e858d118507
Alok kumar yadav <alok953280@gmail.com>
2024-03-25 22:08:06 +0530

    done

---
+++ ecommerce-frontend/src/pages/Shipping.tsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { BiArrowBack } from "react-icons/bi";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { saveShippingInfo } from "../redux/reducer/cartReducer";
+import { RootState, server } from "../redux/store";
+
+const Shipping = () => {
+  const { cartItems, total } = useSelector(
+    (state: RootState) => state.cartReducer
+  );
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [shippingInfo, setShippingInfo] = useState({
+    address: "",
+    city: "",
+    state: "",
+    country: "",
+    pinCode: "",
+  });
+
+  const changeHandler = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setShippingInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    dispatch(saveShippingInfo(shippingInfo));
+
+    try {
+      const { data } = await axios.post(
+        `${server}/api/v1/payment/create`,
+        {
+          amount: total,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      navigate("/pay", {
+        state: data.clientSecret,
+      });
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    if (cartItems.length <= 0) return navigate("/cart");
+  }, [cartItems]);
