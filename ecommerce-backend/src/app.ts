4b622a6cf61f7e6dc2ff02829440a7845d39b7cf
Alok kumar yadav <alok953280@gmail.com>
2024-04-21 15:02:53 +0530

    done

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
-// import { connectDB } from "./utils/features.js";
-// import { errorMiddleware } from "./middlewares/error.js";
-// import NodeCache from "node-cache";
+import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import NodeCache from "node-cache";
 // import { config } from "dotenv";
 // import morgan from "morgan";
 // import Stripe from "stripe";
@@ -18,14 +18,14 @@ import productRoute from "./routes/products.js";
 //   path: "./.env",
 // });
 
-// const port = process.env.PORT || 4000;
-// const mongoURI = process.env.MONGO_URI || "";
+const port = process.env.PORT || 4000;
+const mongoURI = process.env.MONGO_URI || "";
 // const stripeKey = process.env.STRIPE_KEY || "";
 
 connectDB(mongoURI); 
 
 // export const stripe = new Stripe(stripeKey);
-// export const myCache = new NodeCache();
+export const myCache = new NodeCache();
 
 const app = express();
 
@@ -44,7 +44,7 @@ app.use("/api/v1/product", productRoute);
 // app.use("/api/v1/payment", paymentRoute);
 // app.use("/api/v1/dashboard", dashboardRoute);
 
-// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
 
 app.listen(port, () => {
