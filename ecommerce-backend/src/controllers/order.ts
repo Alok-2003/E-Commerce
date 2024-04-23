1ae8c0c7d12e750f9ef32f0f963b4dcbef03d55f
Alok kumar yadav <alok953280@gmail.com>
2024-04-23 12:15:50 +0530

    done

--- ecommerce-backend/src/controllers/order.ts
+++ ecommerce-backend/src/controllers/order.ts
@@ -6,6 +6,50 @@ import { invalidateCache, reduceStock } from "../utils/features.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { myCache } from "../app.js";
 
+export const newOrder = TryCatch(
+  async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
+    const {
+      shippingInfo,
+      orderItems,
+      user,
+      subtotal,
+      tax,
+      shippingCharges,
+      discount,
+      total,
+    } = req.body;
+
+    if (!shippingInfo || !orderItems || !user || !subtotal || !tax ||  !total)
+      return next(new ErrorHandler("Please Enter All Fields", 400));
+
+    const order = await Order.create({
+      shippingInfo,
+      orderItems,
+      user,
+      subtotal,
+      tax,
+      shippingCharges,
+      discount,
+      total,
+    });
+
+    await reduceStock(orderItems);
+
+    invalidateCache({
+      product: true,
+      order: true,
+      admin: true,
+      userId: user,
+      productId: order.orderItems.map((i) => String(i.productId)),
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Order Placed Successfully",
+    });
+  }
+); 
+
 export const myOrders = TryCatch(async (req, res, next) => {
   const { id: user } = req.query;
 
@@ -59,50 +103,6 @@ export const getSingleOrder = TryCatch(async (req, res, next) => {
     order,
   });
 });
-
-export const newOrder = TryCatch(
-  async (req: Request<{}, {}, NewOrderRequestBody>, res, next) => {
-    const {
-      shippingInfo,
-      orderItems,
-      user,
-      subtotal,
-      tax,
-      shippingCharges,
-      discount,
-      total,
-    } = req.body;
-
-    if (!shippingInfo || !orderItems || !user || !subtotal || !tax || !total)
-      return next(new ErrorHandler("Please Enter All Fields", 400));
-
-    const order = await Order.create({
-      shippingInfo,
-      orderItems,
-      user,
-      subtotal,
-      tax,
-      shippingCharges,
-      discount,
-      total,
-    });
-
-    await reduceStock(orderItems);
-
-    invalidateCache({
-      product: true,
-      order: true,
-      admin: true,
-      userId: user,
-      productId: order.orderItems.map((i) => String(i.productId)),
-    });
-
-    return res.status(201).json({
-      success: true,
-      message: "Order Placed Successfully",
-    });
-  }
-);
 
 export const processOrder = TryCatch(async (req, res, next) => {
   const { id } = req.params;
